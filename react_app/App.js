0791bd6d78c678d831584629204c6ec5e8728469
wanqing <wanqing.huang@shopee.com>
2020-09-21 15:31:36 +0800

    添加app demo

--- react_app/App.js
+++ react_app/App.js
@@ -6,24 +6,65 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from 'react';
+import { Platform,
+    StyleSheet,
+    Text,
+    View,
+    Button,
+    NativeModules,
+    Alert,
+    NativeEventEmitter
+} from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
+  android: 'Double tap R on your keyboard to reload,\n' +
     'Shake or press menu button for dev menu',
 });
 
+const Toast = NativeModules.Toast;
+const Pop = NativeModules.Pop;
+
 type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component {
+  componentDidMount(){
+    const eventEmitter = new NativeEventEmitter();
+    eventEmitter.addListener('viewWillAppear', (event) => {
+        console.log("on receive event: viewWillAppear")
+    });
+    eventEmitter.addListener('viewWillReappear', (event) => {
+        console.log("on receive event: viewWillReappear")
+    });
+    eventEmitter.addListener('viewDidDisappear', (event) => {
+        console.log("on receive event: viewDidDisappear")
+    });
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
+        <Button style={{marginTop: 0}}
+            onPress={() => {
+                Toast.show("toast demo", Toast.SHORT);
+            }}
+            title="show a toast"
+        />
+        <Button style={{marginTop: 0}}
+            onPress={ () => {
+            	Pop.showPop("ConfirmDialog",
+            		"{\"content\":\"xxxxxxx\",\"iconType\":0,\"negativeTxt\":\"取消\"" +
+            		",\"positiveTxt\":\"确定\",\"title\":\"xxxxxxxxx\"}",
+                    1000,
+                    () => {console.log("positive")},
+                    () => {console.log("negative")},
+                )
+            }}
+            title="show a toast"
+        />
       </View>
     );
   }
